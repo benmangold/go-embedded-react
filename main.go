40f36e07046462db988cea7ce917a94abd56f40b
benmangold <benmangold@gmail.com>
2021-05-19 22:28:10 -0400

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "embed"
+    "io/fs"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    useOS := len(os.Args) > 1 && os.Args[1] == "live"
+    http.Handle("/", http.FileServer(getFileSystem(useOS)))
+    http.ListenAndServe(":8888", nil)
+}
+
+//go:embed ui/build
+var embededFiles embed.FS
+
+func getFileSystem(useOS bool) http.FileSystem {
+    if useOS {
+        log.Print("using live mode")
+        return http.FS(os.DirFS("static"))
+    }
+
+    log.Print("using embed mode")
+    fsys, err := fs.Sub(embededFiles, "ui/build")
+    if err != nil {
+        panic(err)
+    }
+
+    return http.FS(fsys)
+}
